d9d144982a2986377b91220a3e79708b26490e7f
KJ Wilson <yourboykjdev@gmail.com>
2020-01-06 07:26:35 -0800

    started budget model

---
+++ app/main/model/budget.py
@@ -0,0 +1,76 @@
+from .. import db, flask_bcrypt
+import datetime
+from app.main.model.blacklist import BlacklistToken
+from 
+from ..config import key
+import jwt
+
+# Budget class inherits from db.Model class which declares the class as a model for sqlalchemy
+class Budget(db.Model):
+    """ Budget Model for storing budget related details """
+    __tablename__ = "budget"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    set_on = db.Column(db.DateTime, nullable=False)
+    # whether or not user spent less than amount
+    success = db.Column(db.Boolean, nullable=False, default=False)
+    public_id = db.Column(db.String(100), unique=True)
+    fname = db.Column(db.String(100), unique=True)
+    lname = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password_hash = db.Column(db.String(100))
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    # password.setter uses flask-bcrypt to generate a hash using the provided password.
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+    @staticmethod
+    def encode_auth_token(user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                # returns user_id
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
---
+++ app/main/util/dto.py
@@ -0,0 +1,19 @@
+from flask_restplus import Namespace, fields
+
+class AuthDto:
+    api = Namespace('auth', description='authentication operations')
+    user_auth = api.model('auth', {
+        'email': fields.String(required=True, description='The email address'),
+        'password': fields.String(required=True, description='The user password '),
+    })
+    
+class UserDto:
+    api = Namespace('user', description='user operations')
+    user = api.model('user', {
+        'fname': fields.String(required=True, description='user first name'),
+        'lname': fields.String(required=True, description='user last name'),
+        'email': fields.String(required=True, description='user email address'),
+        'password': fields.String(required=True, description='user password')
+        # TODO: plaid token
+        # TODO: phone numbers for texting updates
+    })
